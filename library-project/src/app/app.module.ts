1444fce9a2271071a49f0faf0cdc3e8224d2da57
saragajic115 <saragajic115@gmail.com>
2020-05-23 21:09:54 +0200

    Added book components, and created header

--- library-project/src/app/app.module.ts
+++ library-project/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BooksComponent } from './books/books.component';
+import { BookListComponent } from './books/book-list/book-list.component';
+import { BookDetailsComponent } from './books/book-details/book-details.component';
+import { BookItemComponent } from './books/book-list/book-item/book-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BooksComponent,
+    BookListComponent,
+    BookDetailsComponent,
+    BookItemComponent
   ],
   imports: [
     BrowserModule,
